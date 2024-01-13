eaaf50316d8147cf8bec26f2b93839145482f4c8
rofifr <whoexaltsjune25@gmail.com>
2024-01-13 22:43:09 +0700

    nambahin info

--- scrollReveal.js
+++ scrollReveal.js
@@ -1,1 +1,2 @@
 ScrollReveal().reveal(".home", { delay: 300, duration: 1000, easing: "ease-in", distance: "50px" });
+ScrollReveal().reveal(".info", { delay: 500, duration: 2000, easing: "ease-in", distance: "50px", origin: "left" });
